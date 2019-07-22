2e84657defdcd137febc4579a9ed00672f8269d5
JessiePM <46710426+JessiePM@users.noreply.github.com>
2019-07-22 20:41:25 +0200

    Update ajaxUser.php

--- ajaxUser.php
+++ ajaxUser.php
@@ -1,8 +1,6 @@
-
 <?php
 // connexion à la base de données
 $db = new PDO('mysql:host=localhost;charset=utf8;dbname=Test', 'Test', 'Test', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
 $term = $_GET['term'];
 
 $requete = $db->prepare('SELECT userName FROM salaries WHERE userName LIKE :term'); // requête SQL grâce au mot-clé LIKE
@@ -14,6 +12,5 @@ while($donnee = $requete->fetch()) // boucle pour obtenir les données
 {
     array_push($array, $donnee['userName']); // ajout au tableau
 }
-
 echo json_encode($array); // conversion en JSON
 ?>
